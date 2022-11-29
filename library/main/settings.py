657390ee83a53a220b124ea0a2ce14af59dc39c9
Code.Weakness <kolyanaumov462@gmail.com>
2022-11-29 14:41:52 +0700

    index to layout

--- library/main/settings.py
+++ library/main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'zdfghSDGefeWR353T532t2ty23T3T32T23T3tagdrgata3t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
